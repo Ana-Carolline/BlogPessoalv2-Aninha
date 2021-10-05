2dc5e145fcb63a547a384c202403e7dc8beffd0f
unknown <ana.carolinelr123@gmail.com>
2021-10-05 14:15:09 -0300

    [Component 'inicio' without consumption] - [30/10/2021]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Usuario } from '../model/Usuario';
 import { UsuarioLogin } from '../model/UsuarioLogin';
 
@@ -15,9 +16,15 @@ export class AuthService {
 
   entrar(UsuarioLogin: UsuarioLogin) : Observable<UsuarioLogin>{
     return this.http.put<UsuarioLogin>('http://localhost:8080/todos/credenciais', UsuarioLogin)
-
   }
   cadastrar(usuario : Usuario): Observable<Usuario>{
     return this.http.post<Usuario>('http://localhost:8080/todos/salvar', usuario)
+  }
+  logado(){
+    let ok: boolean = false
+    if (environment.token != ''){
+      ok = true
+    }
+    return ok
   }
 }
